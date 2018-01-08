2b09efb97d488bd80ddfee54461be12f2e2ae3d7
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-01-08 16:33:17 +0700

    save code

--- เรียน/สายรหัส/week1/week1/Form1.cs
+++ เรียน/สายรหัส/week1/week1/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,38 @@ namespace week1
 {
     public partial class Form1 : Form
     {
+        string text = System.IO.File.ReadAllText("data.txt");
+        Student s = new Student();
+        string[] data;
+        public void readdata()
+        {
+            data= text.Split('*');
+        }
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selected = comboBox1.Text;
+            readdata();
+            string[] y1,y2;
+            // MessageBox.Show(selected);
+            //MessageBox.Show(data[0]);
+            foreach(var y in data)
+            {
+                y1 = y.Split(new[] { Environment.NewLine },StringSplitOptions.None);
+               // MessageBox.Show("0 : "+y1[1]);
+                y2 = y1[1].Split(',');
+               // MessageBox.Show("1 :"+y2[0]);
+               if (y2[0].Equals(selected))
+                {
+                    MessageBox.Show(y1[y1.Length-2]);
+                    break;
+                }
+            }
         }
     }
 }
---
+++ เรียน/สายรหัส/week1/week1/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1
+{
+    class Student
+    {
+        public string name;
+        public string nickname;
+        public string b;
+        public string[] p_code;
+        public string[] n_code;
+        public string y;
+    }
+}
