36c7d8cc7676182d567d1636213ea6dde96433c8
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-01-17 16:55:51 +0700

    save สายรหัส lab

--- lab/Lab1/src/lab1/Lab1.java
+++ lab/Lab1/src/lab1/Lab1.java
@@ -15,8 +15,13 @@ public class Lab1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World !!"); // แสดงผลและขึ้นบรรทัดใหม่
-        System.out.print("Hello World !!");
+       /* System.out.println("Hello World !!"); // แสดงผลและขึ้นบรรทัดใหม่
+        System.out.print("Hello World !!");*/
+       System.out.print("ชื่อ นายวรรณพงษ์  ภัททิยไพบูลย์  ชื่อเล่น ต้นตาล สาขา CIS\n" +
+"พี่รหัสชื่อ นางสาวชลรัตน์ มาทา ชื่อ พี่อ้อย\n" +
+"ลุงรหัสชื่อ นายธีรพัฒน์ เทศชารี ชื่อ ลุงต้น\n" +
+"ป้ารหัสชื่อ นางสาวบุปผา มณีแสง ชื่อ ป้ามิ้ม\n" +
+"ปูรหัสชื่อ นายองอาจ คำจันทร์ ชื่อ ปูนาน");
     }
     
 }
