2baaf6b21459c2f8f9beb5d5f06524a210c763fe
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-02-07 16:16:57 +0700

    add class

---
+++ Lab4/Lab4_University/src/lab4_university/Staff.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_university;
+
+/**
+ *
+ * @author wannaphong
+ */
+public class Staff {
+    String name;
+    int age;
+    
+    public Staff(){
+        
+    }
+    public Staff(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+    public void introduce(){
+        
+    }
+}
