1c3c888a209a79ec4a98a25ebe8dd683d91d7af2
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-03-14 23:17:10 +0700

    save file

---
+++ Lab7_603410214-3/Lab7_603410214-3/src/lab7_603410214/pkg3/Lab7_6034102143.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7_603410214.pkg3;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author wannaphong
+ */
+public class Lab7_6034102143 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Form t = new Form();
+        t.setTitle("Hi");
+        t.setBackground(Color.yellow);
+        t.setVisible(true);
+    }
+    
+}
