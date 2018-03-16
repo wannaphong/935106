1a95118074945ae45d2c2de94d495a6f1e00d717
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-03-16 14:56:11 +0700

    save data

---
+++ Lab7_part2_603410214-3/src/lab7_part2_603410214/pkg3/Lab7_part2_6034102143.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab7_part2_603410214.pkg3;
+
+/**
+ *
+ * @author wannaphong
+ */
+public class Lab7_part2_6034102143 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cal c =new Cal();
+        c.setVisible(true);
+    }
+    
+}
