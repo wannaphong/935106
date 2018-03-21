e9922418d5d2c8244b1d0083c01502ba6bff7cd1
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2018-03-21 17:24:31 +0700

    save file client

--- Lab8_603410214-3/src/lab8_603410214/pkg3/chat_client.java
+++ Lab8_603410214-3/src/lab8_603410214/pkg3/chat_client.java
@@ -46,6 +46,11 @@ public class chat_client extends javax.swing.JFrame {
         jScrollPane1.setViewportView(jTextArea1);
 
         jButton1.setText("sent");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -75,6 +80,15 @@ public class chat_client extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        try{
+            String magout=jTextField1.getText();
+            dout.writeUTF(magout);
+        }
+        catch(Exception e){}
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
